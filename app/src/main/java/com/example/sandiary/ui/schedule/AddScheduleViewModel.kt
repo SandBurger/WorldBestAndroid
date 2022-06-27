24d1f8b6b9b67f86e6b275f16764ae92c248b6dd
duck <gyeondeo@gmail.com>
2022-06-27 19:10:05 +0900

    add TimePicker, DatePicker

--- app/src/main/java/com/example/sandiary/ui/schedule/AddScheduleViewModel.kt
+++ app/src/main/java/com/example/sandiary/ui/schedule/AddScheduleViewModel.kt
@@ -12,9 +12,6 @@ class AddScheduleViewModel : ViewModel() {
         val date = localDate.format(DateTimeFormatter.ofPattern("YYYY년 MM월 dd일"))
         value = date
     }
-    private val _date = MutableLiveData<String>().apply {
 
-    }
     val text: LiveData<String> = _text
-    val date: LiveData<String> = _date
 }
