6add3b8503c2ae76aadbb5c98faf22549d464aa8
duck <gyeondeo@gmail.com>
2022-07-01 19:28:20 +0900

    Change Font, Modify fragment_calendar

--- app/src/main/java/com/example/sandiary/SeeAllActivity.kt
+++ app/src/main/java/com/example/sandiary/SeeAllActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.util.Log
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
